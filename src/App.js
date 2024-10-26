5ff884df5df2c159e9a81c49722dd1f10b6259a5
chrissergio96 <safou.christiansergio@gmail.com>
2024-10-26 13:07:08 +0100

    Christian

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Christian from './Christian/Christian'
 
 function App() {
   return (
     <div className="App">
      <h1>go!</h1>
+     <Christian/>
     </div>
   );
 }
