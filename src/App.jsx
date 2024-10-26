ddb2cab33e4a5fa5c7a2234da4b63ac17b24225e
chrissergio96 <safou.christiansergio@gmail.com>
2024-10-26 13:47:42 +0100

    christian

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Benie from './Benie/Benie'
+import Yohann from './Yohann/Yohann'
 import Christian from './Christian/Christian'
 
 function App() {
   return (
     <div className="App">
      <h1>go!</h1>
+     <Yohann/>
+     <Benie/>
      <Christian/>
     </div>
   );
